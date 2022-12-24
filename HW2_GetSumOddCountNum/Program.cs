e99dc32cd7ffc26f2cbb383d9ac41fe7a3529129
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-25 00:43:50 +0300

    HW2_GetSumOddCountNum

---
+++ HW2_GetSumOddCountNum/Program.cs
@@ -0,0 +1,21 @@
+﻿// вывести количество сумму элементов на нечётных позициях
+// в рандомном массиве из чисел в диапазоне [-99, 99]
+Console.WriteLine("Введите длину массива");
+int lehgth = Convert.ToInt32(Console.ReadLine());
+int[] arr00 = new int[lehgth];
+int SumOddCount = 0;
+
+for (int i = 0; i < arr00.Length; i++)
+  {
+    arr00[i] = new Random().Next(100, 1000);
+  }
+
+ for (int j = 0; j < arr00.Length; j++)
+  {
+    if(j % 2 == 1) SumOddCount = SumOddCount + arr00[j];
+  }
+
+Console.WriteLine(String.Join((", "), (arr00)));
+Console.WriteLine();
+Console.WriteLine("Сумма элементов на нечётных позициях в массиве равна " + SumOddCount);
+
