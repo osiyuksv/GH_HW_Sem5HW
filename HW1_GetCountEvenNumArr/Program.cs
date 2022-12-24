442660e7b8adb85351fe315feb5cccf4a44fa10b
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-25 00:26:08 +0300

    HW1_GetCountEvenNumArr

---
+++ HW1_GetCountEvenNumArr/Program.cs
@@ -0,0 +1,21 @@
+﻿// вывести количество чётных элементов
+// в рандомном массиве из трёхзначных чисел
+Console.WriteLine("Введите длину массива");
+int lehgth = Convert.ToInt32(Console.ReadLine());
+int[] arr000 = new int[lehgth];
+int count = 0;
+
+for (int i = 0; i < arr000.Length; i++)
+  {
+    arr000[i] = new Random().Next(100, 1000);
+  }
+
+ for (int j = 0; j < arr000.Length; j++)
+  {
+    if(arr000[j] % 2 == 0) count ++;
+  }
+
+Console.WriteLine(String.Join((", "), (arr000)));
+Console.WriteLine();
+Console.WriteLine("Число чётных элементов в массиве равно " + count);
+
